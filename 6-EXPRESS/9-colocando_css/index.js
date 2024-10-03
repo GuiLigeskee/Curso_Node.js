70fd91ff48ab2ded6bea78e556d8827c04150afb
Guilherme Saldanha <guiligeskee@gmail.com>
2024-10-03 16:38:57 -0300

    curso node.js dia 03/10/24

---
+++ 6-EXPRESS/9-colocando_css/index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+const path = require("path");
+
+const usersRoutes = require("./users");
+
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+app.use(express.json());
+
+// arquivos estáticos
+app.use(express.static("public"));
+
+const basePath = path.join(__dirname, `templates`);
+
+app.use("/users", usersRoutes);
+
+app.get("/", (req, res) => {
+  res.sendFile(`${basePath}/index.html`);
+});
+
+app.listen(port, () => {
+  console.log(`App rodando na porta ${port}`);
+});
